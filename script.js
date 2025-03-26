9583760f23c3e48d1602fbc742754dfb5f4cb1ce
dale-nguyen <dalenguyen.thatera@gmail.com>
2025-03-26 14:37:53 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple JavaScript functionality
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Page loaded. Test out the navigation and dead buttons for rage clicking.");
+  });
+  
