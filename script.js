1c8c1439c702bf67e577a8f90608222800217be5
dale-nguyen <dalenguyen.thatera@gmail.com>
2025-08-06 11:43:03 +0700

    Add custom event

--- script.js
+++ script.js
@@ -3,4 +3,16 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log(
     "Page loaded. Test out the navigation and dead buttons for rage clicking."
   );
+  const btn = document.querySelector("#dead-button2"); // Using ID selector
+  if (btn) {
+    btn.addEventListener("click", function () {
+      if (window.clarity) {
+        window.clarity("event", "DeadButton2Clicked", {
+          reason: "user clicked second button",
+          source: "HTML button",
+        });
+      }
+      alert("This button is also dead!");
+    });
+  }
 });
