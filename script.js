73c3ce63b065d5dd181218ccb6c7c287c5a68515
dale-nguyen <dalenguyen.thatera@gmail.com>
2025-08-06 10:07:14 +0700

    Fix 2

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 // Simple JavaScript functionality
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("Page loaded. Test out the navigation and dead buttons for rage clicking.");
-  });
-  
+  console.log(
+    "Page loaded. Test out the navigation and dead buttons for rage clicking."
+  );
+  mixpanel.init("958b76b27302f332f6244250401a4825", { debug: true });
+
+  mixpanel.track("Page Loaded");
+
+  const btn = document.getElementById("dead-button");
+  if (btn) {
+    btn.addEventListener("click", () => {
+      mixpanel.track("Clicked Button", {
+        source: "HTML test project",
+      });
+    });
+  }
+});
