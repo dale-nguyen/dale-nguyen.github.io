4b737d04487dae1222238b1edc2630708c0b8998
dale-nguyen <dalenguyen.thatera@gmail.com>
2025-08-06 10:12:11 +0700

    Fix

--- script.js
+++ script.js
@@ -3,16 +3,4 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log(
     "Page loaded. Test out the navigation and dead buttons for rage clicking."
   );
-  mixpanel.init("abc27fde1962f75d1741d0094f77e7b6", { debug: true });
-
-  mixpanel.track("Page Loaded");
-
-  const btn = document.getElementById("dead-button");
-  if (btn) {
-    btn.addEventListener("click", () => {
-      mixpanel.track("Clicked Button", {
-        source: "HTML test project",
-      });
-    });
-  }
 });
